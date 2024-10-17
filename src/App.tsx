9e6618fafc1c22caea15e4005d70dd8c879c5a60
Brendon Mendicino <brendonmendicino@yahoo.it>
2024-10-17 23:05:25 +0200

    feat: added theming

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import ProfessionalPage from "./pages/ProfessionalPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ProfessionalsPage from "./pages/ProfessionalsPage";
 import CreateProfessionalPage from "./pages/CreateProfessionalPage";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { crmTheme } from "./theme/theme.ts";
 
 const queryClient = new QueryClient();
 
@@ -14,23 +16,27 @@ export function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <UserContext.Provider value={user}>
-          <Routes>
-            <Route path="/ui" element={<HomePage />}>
-              <Route path="professionals" element={<ProfessionalsPage />} />
-              <Route
-                path="professionals/:professionalId"
-                element={<ProfessionalPage />}
-              />
-              <Route
-                path="professionals/create"
-                element={<CreateProfessionalPage />}
-              />
-            </Route>
-          </Routes>
-        </UserContext.Provider>
-      </Router>
+      <ThemeProvider theme={crmTheme}>
+        <CssBaseline>
+          <Router>
+            <UserContext.Provider value={user}>
+              <Routes>
+                <Route path="/ui" element={<HomePage />}>
+                  <Route path="professionals" element={<ProfessionalsPage />} />
+                  <Route
+                    path="professionals/:professionalId"
+                    element={<ProfessionalPage />}
+                  />
+                  <Route
+                    path="professionals/create"
+                    element={<CreateProfessionalPage />}
+                  />
+                </Route>
+              </Routes>
+            </UserContext.Provider>
+          </Router>
+        </CssBaseline>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
